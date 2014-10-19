990764cf84c326267985b3f6bedc8fb7943ad7f8
Luthaf <luthaf@luthaf.fr>
2014-10-19 22:33:52 +0200

    Run tests under Python 2 and Python 3

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,4 +3,8 @@
 TESTS_DIR=$(dirname $0)
 export PYTHONPATH=$TESTS_DIR/..:$TESTS_DIR
 
-django-admin.py test background_task --settings=test_settings
+echo "=============================== Testing Python2 ==============================="
+python2 `which django-admin.py` test background_task --settings=test_settings
+
+echo "=============================== Testing Python3 ==============================="
+python3 `which django-admin.py` test background_task --settings=test_settings
