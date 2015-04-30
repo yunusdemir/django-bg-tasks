9063d2fc6da99e75036542378d1414809cf1234d
Luthaf <luthaf@luthaf.fr>
2015-04-30 23:39:32 +0200

    We should use virtualenv to handle python versions

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -3,8 +3,4 @@
 TESTS_DIR=$(dirname $0)
 export PYTHONPATH=$TESTS_DIR/..:$TESTS_DIR
 
-echo "=============================== Testing Python2 ==============================="
-python2 `which django-admin.py` test_coverage background_task --settings=test_settings
-
-echo "=============================== Testing Python3 ==============================="
-python3 `which django-admin.py` test_coverage background_task --settings=test_settings
+python `which django-admin.py` test_coverage background_task --settings=test_settings
