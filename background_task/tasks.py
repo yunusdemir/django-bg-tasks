3216964d80884748b56130b973afc8a17e56df3c
Kyrylo Kobets <mjr@itage.biz>
2017-02-02 13:10:11 +0200

    Add a limit of created threads. #26

--- background_task/tasks.py
+++ background_task/tasks.py
@@ -13,15 +13,20 @@ from django.conf import settings
 from compat import atomic
 from compat import import_module
 
+from multiprocessing.pool import ThreadPool
+
 from background_task.exceptions import BackgroundTaskError
 from background_task.models import Task
 from background_task.signals import task_created, task_error, task_successful
-
 
 logger = logging.getLogger(__name__)
 
 
 BACKGROUND_TASK_RUN_ASYNC = getattr(settings, 'BACKGROUND_TASK_RUN_ASYNC', False)
+BACKGROUND_TASK_ASYNC_THREADS = getattr(settings, 'BACKGROUND_TASK_ASYNC_THREADS', None)
+
+
+_thread_pool = ThreadPool(processes=BACKGROUND_TASK_ASYNC_THREADS)
 
 
 def bg_runner(proxy_task, task=None, *args, **kwargs):
@@ -108,8 +113,7 @@ class Tasks(object):
             task = None
         proxy_task = self._tasks[task_name]
         if BACKGROUND_TASK_RUN_ASYNC:
-            curr_thread = threading.Thread(target=self._bg_runner, args=(proxy_task, task) + tuple(args), kwargs=kwargs)
-            curr_thread.start()
+            _thread_pool.apply_async(func=self._bg_runner, args=(proxy_task, task) + tuple(args), kwds=kwargs)
         else:
             self._bg_runner(proxy_task, task, *args, **kwargs)
 
