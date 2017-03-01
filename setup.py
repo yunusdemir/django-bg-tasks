63f6b546503e762faf033d574117ba83748326e9
Walter Renner <renner@arteria.ch>
2017-03-01 15:19:05 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import codecs
 
-version = __import__('background_task').__version__
+version = __import__('background_task').__version__ + "-post1"
 
 classifiers = [c for c in open('classifiers').read().splitlines() if '#' not in c]
 
