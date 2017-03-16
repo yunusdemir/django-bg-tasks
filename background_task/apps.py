788a31f56d101e9cfed6a192973bdf8e40d74ee7
Walter Renner <renner@arteria.ch>
2017-03-16 11:13:21 +0100

    Individual DB connections for each Thread
    close unused db connections when a new thread
    is started and closed

--- background_task/apps.py
+++ background_task/apps.py
@@ -8,3 +8,6 @@ else:
         name = 'background_task'
         from background_task import __version__ as version_info
         verbose_name = 'Background Tasks ({})'.format(version_info)
+
+        def ready(self):
+            import background_task.signals  # noqa
