b8ce7b32505e0ba214f26c26c212c2b2918d3090
Yunus Demir <github@yunusdemir.nl>
2023-12-06 11:13:23 +0100

    Update configuration files for versioning

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ version = __import__('background_task').__version__
 classifiers = [c for c in open('classifiers').read().splitlines() if '#' not in c]
 
 setup(
-    name='django-background-tasks',
+    name='django-bg-tasks',
     version=version,
     description='Database backed asynchronous task queue',
     long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    author='arteria GmbH, John Montgomery',
+    author='Anywyse.audio',
     author_email='admin@arteria.ch',
-    url='http://github.com/arteria/django-background-tasks',
+    url='http://github.com/yunusdemir/django-bg-tasks',
     license='BSD',
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
